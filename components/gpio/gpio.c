6dd276d69b416cd9f5ce6ab98e9411ffb1ec9c15
Matteo-o-o <bosserellematteo@gmail.com>
2026-09-03 15:13:31 +0000

    GPIO implementation

--- components/gpio/gpio.c
+++ components/gpio/gpio.c
@@ -15,7 +15,7 @@ static const gpio_register_t GPIO_BUS[] = {
 };
 
 void gpio_init(gpio_port_t port,gpio_pin_t pin,gpio_mode_t mode){
-    gpio_register_t *bus = &GPIO_BUS[port];
+    const gpio_register_t *bus = &GPIO_BUS[port];
 
     switch (mode)
     {
@@ -31,7 +31,37 @@ void gpio_init(gpio_port_t port,gpio_pin_t pin,gpio_mode_t mode){
     case GPIO_MODE_INPUT_PULLUP:
         // Bit DDRx 0 => entry , bit PORT 1 => Intern Pull Up
         *(bus->ddr) &= ~(1<<pin);
-        *(bus->port) |= (1<pin);
+        *(bus->port) |= (1<<pin);
         break;
     }
+}
+void gpio_write(gpio_port_t port,gpio_pin_t pin,gpio_state_t state){
+    if(state == GPIO_STATE_HIGH){
+        gpio_set_high(port,pin);
+    } else{
+        gpio_set_low(port,pin);
+    }
+}
+void gpio_set_high(gpio_port_t port,gpio_pin_t pin){
+    // Write => Register PORT
+    *(GPIO_BUS[port].port) |= (1<<pin); 
+}
+
+void gpio_set_low(gpio_port_t port,gpio_pin_t pin){
+    *(GPIO_BUS[port].port) &= ~(1<<pin); 
+}
+
+gpio_state_t gpio_read(gpio_port_t port,gpio_pin_t pin){
+
+    if(*(GPIO_BUS[port].pin) & (1<<pin)){
+        return GPIO_STATE_HIGH;
+    } else {
+        return GPIO_STATE_LOW;
+    }
+}
+
+void gpio_toggle(gpio_port_t port,gpio_pin_t pin){
+    *(GPIO_BUS[port].pin) = (1<<pin);
+    // Same :     *(GPIO_BUS[port].pin) = (1<<pin);
+
 }
